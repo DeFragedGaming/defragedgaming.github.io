e5b0461ee4079528eb0c5b5b276d220837aceb65
Matthew Hammel <mattyhammel17@gmail.com>
2026-02-07 06:58:49 -0700

    fixed tiny error passed build run

---
+++ src/pages/labs/password-hashing.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "@theme/Layout";
+
+export default function PasswordHashingLab() {
+  return (
+    <Layout title="Password Hashing Lab">
+      <div style={{ padding: "2rem" }}>
+        <h1>Password Hashing Lab</h1>
+        <p>This lab is under construction.</p>
+      </div>
+    </Layout>
+  );
+}
